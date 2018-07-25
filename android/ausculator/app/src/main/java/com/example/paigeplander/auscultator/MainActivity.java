b634512efbf82eb07a0b6cdeb92da123227546e9
paigeplan <paigeplander@gmail.com>
2018-07-25 01:43:07 -0700

    bluetooth starter app working

--- android/ausculator/app/src/main/java/com/example/paigeplander/auscultator/MainActivity.java
+++ android/ausculator/app/src/main/java/com/example/paigeplander/auscultator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.paigeplander.auscultator;
 
 import android.Manifest;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
@@ -9,16 +11,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BluetoothLeUart.Callback {
 
     private static final String LOG_TAG = "AudioRecordTest";
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
@@ -31,6 +36,63 @@ public class MainActivity extends AppCompatActivity {
     private MediaPlayer mPlayer;
 
     private TextView titleTextView;
+
+
+    // UI elements
+    private TextView messages;
+    private EditText input;
+    private Button   send;
+    private CheckBox newline;
+
+    // Bluetooth LE UART instance.  This is defined in BluetoothLeUart.java.
+    private BluetoothLeUart uart;
+
+    // Write some text to the messages text view.
+    // Care is taken to do this on the main UI thread so writeLine can be called from any thread
+    // (like the BTLE callback).
+    private void writeLine(final CharSequence text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messages.append(text);
+                messages.append("\n");
+            }
+        });
+    }
+
+
+    // Handler for mouse click on the send button.
+    public void sendClick(View view) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String message = input.getText().toString();
+
+        // We can only send 20 bytes per packet, so break longer messages
+        // up into 20 byte payloads
+        int len = message.length();
+        int pos = 0;
+        while(len != 0) {
+            stringBuilder.setLength(0);
+            if (len>=20) {
+                stringBuilder.append(message.toCharArray(), pos, 20 );
+                len-=20;
+                pos+=20;
+            }
+            else {
+                stringBuilder.append(message.toCharArray(), pos, len);
+                len = 0;
+            }
+            uart.send(stringBuilder.toString());
+        }
+        // Terminate with a newline character if requests
+        newline = (CheckBox) findViewById(R.id.newline);
+        if (newline.isChecked()) {
+            stringBuilder.setLength(0);
+            stringBuilder.append("\n");
+            uart.send(stringBuilder.toString());
+        }
+    }
+
+
 
     // Requesting permission to RECORD_AUDIO
     private boolean permissionToRecordAccepted = false;
@@ -153,6 +215,29 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_main);
+
+
+        //** BLUETOOTH LE STUFF **//
+
+
+        // Grab references to UI elements.
+        messages = (TextView) findViewById(R.id.messages);
+        input = (EditText) findViewById(R.id.input);
+
+        // Initialize UART.
+        uart = new BluetoothLeUart(getApplicationContext());
+
+        // Disable the send button until we're connected.
+        send = (Button)findViewById(R.id.send);
+        send.setClickable(false);
+        send.setEnabled(false);
+
+        // Enable auto-scroll in the TextView
+        messages.setMovementMethod(new ScrollingMovementMethod());
+
+
+        //** AUDIO STUFF **//
+
         // Record to the external cache directory for visibility
         mFileName = getExternalCacheDir().getAbsolutePath();
         mFileName += "/audiorecordtest.3gp";
@@ -178,6 +263,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStop() {
         super.onStop();
+        uart.unregisterCallback(this);
+        uart.disconnect();
         if (mRecorder != null) {
             mRecorder.release();
             mRecorder = null;
@@ -187,5 +274,79 @@ public class MainActivity extends AppCompatActivity {
             mPlayer.release();
             mPlayer = null;
         }
+    }
+
+
+    // OnResume, called right before UI is displayed.  Connect to the bluetooth device.
+    @Override
+    protected void onResume() {
+        super.onResume();
+        writeLine("Scanning for devices ...");
+        uart.registerCallback(this);
+        uart.connectFirstAvailable();
+    }
+
+
+    // UART Callback event handlers.
+    @Override
+    public void onConnected(BluetoothLeUart uart) {
+        // Called when UART device is connected and ready to send/receive data.
+        writeLine("Connected!");
+        // Enable the send button
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                send = (Button)findViewById(R.id.send);
+                send.setClickable(true);
+                send.setEnabled(true);
+            }
+        });
+    }
+
+    @Override
+    public void onConnectFailed(BluetoothLeUart uart) {
+        // Called when some error occured which prevented UART connection from completing.
+        writeLine("Error connecting to device!");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                send = (Button)findViewById(R.id.send);
+                send.setClickable(false);
+                send.setEnabled(false);
+            }
+        });
+    }
+
+    @Override
+    public void onDisconnected(BluetoothLeUart uart) {
+        // Called when the UART device disconnected.
+        writeLine("Disconnected!");
+        // Disable the send button.
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                send = (Button)findViewById(R.id.send);
+                send.setClickable(false);
+                send.setEnabled(false);
+            }
+        });
+    }
+
+    @Override
+    public void onReceive(BluetoothLeUart uart, BluetoothGattCharacteristic rx) {
+        // Called when data is received by the UART.
+        writeLine("Received: " + rx.getStringValue(0));
+    }
+
+    @Override
+    public void onDeviceFound(BluetoothDevice device) {
+        // Called when a UART device is discovered (after calling startScan).
+        writeLine("Found device : " + device.getAddress());
+        writeLine("Waiting for a connection ...");
+    }
+
+    @Override
+    public void onDeviceInfoAvailable() {
+        writeLine(uart.getDeviceInfo());
     }
 }
